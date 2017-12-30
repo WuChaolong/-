837b3624161473e0223848c0a7f4026658619fa3
Taifu <752761705@qq.com>
2017-12-30 20:49:11 +0800

    a

--- social-share.js/src/js/social-share.js
+++ social-share.js/src/js/social-share.js
@@ -163,9 +163,9 @@
             return false;
         }
 
-        var elems = createElementByString('<div class="wechat-qrcode"><h4>' + data.wechatQrcodeTitle + '</h4><div class="qrcode"></div><div class="help">' + data.wechatQrcodeHelper + '</div></div>');
+        var elems = createElementByString('<div class="wechat-qrcode"><h4>' + data.wechatQrcodeTitle + '</h4><a class="qrcode" target="_blank"></a><div class="help">' + data.wechatQrcodeHelper + '</div></div>');
         var qrcode = getElementsByClassName(elems[0], 'qrcode', 'div');
-
+        qrcode.href = data.url;
         wechat[0].appendChild(elems[0]);
         new QRCode(qrcode[0], {text: data.url, width: data.wechatQrcodeSize, height: data.wechatQrcodeSize});
     }
